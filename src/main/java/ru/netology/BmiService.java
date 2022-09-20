7afbbff5ec5c9f04aee7de2be0358b3001e00c59
Vladimir Pavlyukov <yahusim@mail.ru>
2022-09-20 22:45:46 +0300

    rework

--- src/main/java/ru/netology/BmiService.java
+++ src/main/java/ru/netology/BmiService.java
@@ -1,52 +1,53 @@
 package ru.netology;
 
 public class BmiService {
-    public Double calculate(Double weight_kg, double height_sm){
-        return weight_kg/((height_sm / 100.0) * (height_sm / 100.0));
+    public double calculate(double weightKg, double heightSm) {
+        return weightKg / ((heightSm / 100.0) * (heightSm / 100.0));
     }
 
-    public String interpretation(Double bmi){
-        if(bmi >= 40.0){
+    public String interpretation(double bmi) {
+        if (bmi >= 40.0) {
             return "\033[91mОжирение 3 степени\033[0m";
         }
-        if(bmi >= 35.0){
-            return "\033[95mОжирение 2 степени\033[0m";
+        if (bmi >= 35.0) {
+            return "\033[31mОжирение 2 степени\033[0m";
         }
-        if(bmi >= 30.0){
+        if (bmi >= 30.0) {
             return "\033[33mОжирение 1 степени\033[0m";
         }
-        if(bmi >= 25.0){
-            return "\033[92mИзбыточная масса тела (предожирение)\033[0m";
+        if (bmi >= 25.0) {
+            return "\033[93mИзбыточная масса тела (предожирение)\033[0m";
         }
-        if(bmi >= 18.5){
-            return "\033[32mНорма\033[0m";
+        if (bmi >= 18.5) {
+            return "\033[92mНорма\033[0m";
         }
-        if(bmi > 16.0){
+        if (bmi > 16.0) {
             return "\033[96mНедостаточная (дефицит) масса тела\033[0m";
         }
-        return "\033[94mВыраженный дефицит массы тела\033[0m";
+        return "\033[34mВыраженный дефицит массы тела\033[0m";
     }
-    public String colorInterpretation(Double weight_kg, double height_sm){
-        Double bmi = calculate(weight_kg, height_sm);
+
+    public String colorInterpretation(double weightKg, double heightSm) {
+        double bmi = calculate(weightKg, heightSm);
         String sbmi = String.format("%.2f", bmi);
-        if(bmi >= 40.0){
-            return "\033[31m%" + sbmi + "\033[0m";
+        if (bmi >= 40.0) {
+            return "\033[91m%" + sbmi + "\033[0m";
         }
-        if(bmi >= 35.0){
-            return "\033[35m" + sbmi + "\033[0m";
+        if (bmi >= 35.0) {
+            return "\033[31m" + sbmi + "\033[0m";
         }
-        if(bmi >= 30.0){
+        if (bmi >= 30.0) {
             return "\033[33m" + sbmi + "\033[0m";
         }
-        if(bmi >= 25.0){
-            return "\033[36m" + sbmi + "\033[0m";
+        if (bmi >= 25.0) {
+            return "\033[93m" + sbmi + "\033[0m";
         }
-        if(bmi >= 18.5){
-            return "\033[32m" + sbmi + "\033[0m";
+        if (bmi >= 18.5) {
+            return "\033[92m" + sbmi + "\033[0m";
         }
-        if(bmi > 16.0){
-            return "\033[34m" + sbmi + "\033[0m";
+        if (bmi > 16.0) {
+            return "\033[96m" + sbmi + "\033[0m";
         }
-        return "\033[35m" + sbmi + "\033[0m";
+        return "\033[34m" + sbmi + "\033[0m";
     }
 }
--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -5,13 +5,12 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         BmiService bmiService = new BmiService();
-        int len = 10;
+        int len = 100;
         Random random = new Random(System.currentTimeMillis());
-        for(int i = 0; i < len; i++){
-            Double weight = 76 + random.nextDouble()*(200 - 50);
-            Double heights = 170 + random.nextDouble()*(210-120);
-
-            System.out.printf("Для веса %.2f и роста %.2f индекс массы составляет ", weight,heights);
+        for (int i = 0; i < len; i++) {
+            double weight = 50 + random.nextDouble() * (200 - 50);
+            double heights = 120 + random.nextDouble() * (200 - 120);
+            System.out.printf("Для веса %.2f и роста %.2f индекс массы составляет ", weight, heights);
             System.out.println(bmiService.colorInterpretation(weight, heights));
         }
     }
